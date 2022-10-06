1fdcb5fcf089d89eefc96a98395a569f9908a734
Nimrod Aviram <nimrod.aviram@gmail.com>
2022-10-06 13:33:23 +0300

    Minor edit to generate_pastebin.sh.

--- generate_pastebin.sh
+++ generate_pastebin.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
-tail -n +1 *.c *.h *.py go.sh benchmark.sh > pastebin.txt
+echo "Compiled with OpenSSL 1.1.1" > pastebin.txt
+tail -n +1 *.c *.h *.py go.sh benchmark.sh >> pastebin.txt
 
 # Make sure the resulting file is anonymized
 grep -i nimrod pastebin.txt
